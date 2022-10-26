b8906b0b27ca68ff5d1e76c59e0e631557f98157
Goraie <90771182+Goraie@users.noreply.github.com>
2022-10-26 05:30:09 +0300

    done privacy

--- src/js/index.js
+++ src/js/index.js
@@ -10,13 +10,42 @@ rateBtns.forEach(item => {
 	})
 })
 
-const sitesAll = document.querySelectorAll('.sites__row'),
-			desktopCarousel = document.querySelector('.sites__carousel'),
-			mobileCarousel = document.querySelector('.sites__mobile')
+// const sitesAll = document.querySelectorAll('.sites__row'),
+// 			desktopCarousel = document.querySelector('.sites__carousel'),
+// 			mobileCarousel = document.querySelector('.sites__mobile')
 
-// window.addEventListener('resize', (e) => runSitesSlider())
+// // window.addEventListener('resize', (e) => runSitesSlider())
 
-function runSitesSlider(){
+// function runSitesSlider(){
+// }
+
+// // 
+
+function animateSlider(el, duration, dir) {
+  const innerEl = el.querySelector('.sites__carousel');
+  const innerWidth = innerEl.offsetWidth;
+  const cloneEl = innerEl.cloneNode(true);
+  el.appendChild(cloneEl);
+  let start = performance.now();
+  let progress;
+  let translateX;
+  let translateY;
+  requestAnimationFrame(function step(now) {
+    progress = (now - start) / duration;
+    if (progress > 1) {
+    	progress %= 1;
+      start = now;
+    }
+    translateX = innerWidth * progress;
+    translateY = innerHeight * progress;
+    if(dir === 'v'){
+			innerEl.style.transform = `translate3d(0,-${translateY}px , 0)`;
+			cloneEl.style.transform = `translate3d(0,-${translateY}px , 0)`;
+		} else if(dir ==='h'){
+			innerEl.style.transform = `translate3d(-${translateX}px, 0 , 0)`;
+			cloneEl.style.transform = `translate3d(-${translateX}px, 0 , 0)`;
+		}
+    requestAnimationFrame(step);
+  });
 }
-
-console.log(sitesAll);
+animateSlider(document.querySelector('.sites__slider-w'), 20000, window.innerWidth > 992 ? 'v' : 'h' );
