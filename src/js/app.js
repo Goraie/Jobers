7561ad6041224ef23deec3b85d4dfa1c4dd32275
Goraie <90771182+Goraie@users.noreply.github.com>
2022-10-31 14:27:58 +0300

    add swiper

--- src/js/app.js
+++ src/js/app.js
@@ -288,3 +288,35 @@ function showIframe(i){
 			addRemoveReportsClass(false , i)
 	})
 }
+
+const postExWrap = document.querySelector('.postEx__wrapp'),
+			postExSlides = postExWrap.querySelectorAll('.swiper-slide'),
+			postExClose = document.querySelector('.postEx__exit'),
+			postExSwiper = document.querySelector('.postEx__swiper')
+
+const postExArray = [postExSwiper.offsetHeight, postExSwiper.offsetWidth]
+
+postExSlides.forEach(item => {
+	item.addEventListener('click', (e) => {
+		postExWrap.classList.add('active')
+		calcSizes(postExSwiper)
+		document.querySelector('body').style.overflow = 'hidden'
+	})
+})
+
+
+function calcSizes (elem) {
+	const height = postExWrap.offsetHeight
+	const width = height * 0.9
+	setSizes(elem, [height,width])
+}
+function setSizes (item, [h,w]) {
+	item.style.height = h + 'px'
+	item.style.width = w + 'px'
+}
+
+postExClose.addEventListener('click', () => {
+	postExWrap.classList.remove('active')
+	setSizes(postExSwiper, postExArray)
+	document.querySelector('body').style.overflow = 'visible'
+})
