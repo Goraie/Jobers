c8e13ed593eef030d3b92f68006384c981fbb477
Goraie <90771182+Goraie@users.noreply.github.com>
2022-10-31 13:17:12 +0300

    try

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -27,17 +27,17 @@ export const images = () =>{
 								app.plugins.newer(app.path.build.images)
 							)
 						)
-						.pipe(
-							app.plugins.if(
-								app.isBuild,
-								imagemin({
-									progressive: true,
-									svgoPlugins: [{removeViewBox: false}],
-									interlaced: true,
-									optimizationLavel: 3 // 0 - 7
-								})
-							)
-						)
+						// .pipe(
+						// 	app.plugins.if(
+						// 		app.isBuild,
+						// 		imagemin({
+						// 			progressive: true,
+						// 			svgoPlugins: [{removeViewBox: false}],
+						// 			interlaced: true,
+						// 			optimizationLavel: 3 // 0 - 7
+						// 		})
+						// 	)
+						// )
 						.pipe(app.gulp.dest(app.path.build.images))
 						.pipe(app.gulp.src(app.path.src.svg))
 						.pipe(app.gulp.dest(app.path.build.images))
